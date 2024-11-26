291ef075abacc999ec2d0741992ffc4a0703ae20
eulogiomedina <134175832+eulogiomedina@users.noreply.github.com>
2024-11-26 10:45:25 -0600

    ajustes de usefect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext'; // Proveedor de contexto de autenticación
 import Header from './components/Header';
