dd6df536292c1a204192ccdb10923ee1bc4e8ad1
eulogiomedina <134175832+eulogiomedina@users.noreply.github.com>
2024-11-26 11:42:22 -0600

    ajustes footer

--- src/components/SocialLinksManager.js
+++ src/components/SocialLinksManager.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';//corrigiendo
+import axios from 'axios';
+import '../styles/SocialLinksManager.css'
 
 const SocialLinksManager = () => {
     const [links, setLinks] = useState([]);
