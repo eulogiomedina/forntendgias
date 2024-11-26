5ffdee91992bb72a642aeeaa98af47854d50cb1d
eulogiomedina <134175832+eulogiomedina@users.noreply.github.com>
2024-11-26 09:03:41 -0600

    ajustes de boostrap

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext'; // Proveedor de contexto de autenticación
